14b0e8cdb069c75c57f497c896f5fcc7547cb924
nikitosing <32202610+nikitosing@users.noreply.github.com>
2018-07-16 12:01:22 +1000

    Update ForeignBridge.sol

--- ForeignBridge.sol
+++ ForeignBridge.sol
@@ -10,12 +10,22 @@
     
     contract ForeignBridge{
         
-        constructor(address _addr) public{
+        constructor(address _addr, address[] _valiodators, uint _requredSignature) public{
             homeContractAddr = _addr;
+            requiredSignatures = _requredSignature;
+            for (uint i = 0; i < _valiodators.length; i++){
+                isValidator[_valiodators[i]] = true;
+            }
         }
         
         address homeContractAddr;
+        uint requiredSignatures;
         
+        mapping (address => bool) isValidator;
+        mapping (bytes32 => bool) isTokenRecovered;
+        mapping (bytes32 => bool) isValidatorAlrHanded;
+        mapping (bytes32 => uint) countForRecovery;
+            
         ForeignToken foreignToken = ForeignToken(homeContractAddr);
         
         event userRequestForSignature(uint _tokenId, bytes[] _tokenData);
@@ -28,8 +38,23 @@
         
         event transferCompleted(uint _tokenId);
         
-        function transferApproved(address _owner, uint _tokenId, bytes[] _tokenData){
-            foreignToken.recoveryToken(_tokenId, _tokenData);
-            foreignToken.transfer(_tokenId, _owner);
-            emit transferCompleted(_tokenId);    }
+        function transferApproved(address _owner, uint _tokenId, bytes[] _tokenData, bytes32 _txHash){
+            require(isValidator[msg.sender]);
+            require(!isTokenRecovered[_txHash]);
+            bytes32 hash = keccak256(abi.encodePacked(_txHash, msg.sender));
+            require(!isValidatorAlrHanded[hash]);
+            isValidatorAlrHanded[hash] = true;
+            bytes memory _tokenData1;
+            for (uint i = 0; i < _tokenData.length; i++){
+                _tokenData1 =  abi.encodePacked(_tokenData1, _tokenData[i]);
+            }
+            hash = keccak256(abi.encodePacked(_txHash, msg.sender, _tokenId, _tokenData1));
+            countForRecovery[hash]++;
+            if(countForRecovery[hash] >= requiredSignatures){
+                foreignToken.recoveryToken(_tokenId, _tokenData);
+                foreignToken.transfer(_tokenId, _owner);
+                isTokenRecovered[_txHash] = true;
+                emit transferCompleted(_tokenId);    
+            }
+        }
     }

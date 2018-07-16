89be3f0591cd649211c512b567db739bcabe2606
nikitosing <32202610+nikitosing@users.noreply.github.com>
2018-07-16 12:00:28 +1000

    Update HomeBridge.sol

--- HomeBridge.sol
+++ HomeBridge.sol
@@ -9,15 +9,25 @@ contract HomeToken{
 
 contract HomeBridge{
     
-    constructor(address _addr) public{
+    constructor(address _addr, address[] _valiodators, uint _requredSignature) public{
         homeContractAddr = _addr;
+        requiredSignatures = _requredSignature;
+        for (uint i = 0; i < _valiodators.length; i++){
+            isValidator[_valiodators[i]] = true;
+        }
     }
     
     address homeContractAddr;
+    uint requiredSignatures;
+    
+    mapping (address => bool) isValidator;
+    mapping (bytes32 => bool) isTokenRecovered;
+    mapping (bytes32 => bool) isValidatorAlrHanded;
+    mapping (bytes32 => uint) countForRecovery;
     
     HomeToken homeToken = HomeToken(homeContractAddr);
     
-    event userRequestForSignature(uint _tokenId, bytes[] _tokenData);
+    event userRequestForSignature(address _from, uint _tokenId, bytes[] _tokenData);
     
     function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes _data) external returns(bytes4){
         emit userRequestForSignature(_from, _tokenId, homeToken.getSerializedData(_tokenId));
@@ -26,10 +36,24 @@ contract HomeBridge{
     
     event transferCompleted(uint _tokenId);
     
-    function transferApproved(address _owner, uint _tokenId, bytes[] _tokenData){
-        homeToken.recoveryToken(_tokenId, _tokenData);
-        homeToken.transfer(_tokenId, _owner);
-        emit transferCompleted(_tokenId);    
+    function transferApproved(address _owner, uint _tokenId, bytes[] _tokenData, bytes32 _txHash){
+        require(isValidator[msg.sender]);
+        require(!isTokenRecovered[_txHash]);
+        bytes32 hash = keccak256(abi.encodePacked(_txHash, msg.sender));
+        require(!isValidatorAlrHanded[hash]);
+        isValidatorAlrHanded[hash] = true;
+        bytes memory _tokenData1;
+        for (uint i = 0; i < _tokenData.length; i++){
+            _tokenData1 =  abi.encodePacked(_tokenData1, _tokenData[i]);
+        }
+        hash = keccak256(abi.encodePacked(_txHash, msg.sender, _tokenId, _tokenData1));
+        countForRecovery[hash]++;
+        if(countForRecovery[hash] >= requiredSignatures){
+            homeToken.recoveryToken(_tokenId, _tokenData);
+            homeToken.transfer(_tokenId, _owner);
+            isTokenRecovered[_txHash] = true;
+            emit transferCompleted(_tokenId);    
+        }
     }
     
 }
